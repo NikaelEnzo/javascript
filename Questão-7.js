29273da0e2b6a7fe97c26f53f46a67bfd59a8849
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-13 17:27:19 -0400

    Questão 8 concluida

--- Questão-7.js
+++ Questão-7.js
@@ -2,6 +2,4 @@
 Questão 7: Como você pode declarar e atribuir um valor a uma variável em uma única linha em JavaScript?
 */
 
-let num1 = 1; 
-
-                         
+let num1 = 1;
---
+++ Questão-8.js
@@ -0,0 +1,5 @@
+/*
+Questão 8: Como você pode acessar o último elemento de um array em JavaScript?
+*/
+
+let arrayExemplo = [1, 2, 3, 4, 5]; // "arrayExemplo" é um array por armazena uma coleção de elementos, sendo geralmente do mesmo tipo
