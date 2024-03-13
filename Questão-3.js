ec8c45ca801fda65614256bf1803e7c873c0b39b
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-13 16:46:55 -0400

    Questão 3 concluida

---
+++ Questão-3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript? 
+*/
+
+let nome = "nika"
+let son = "enzo"
+let resultado
+
+resultado = nome + " " + son
+console.log(resultado);
