130552eb424a76a758bb0eb79ff76e5338f2fbf5
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-18 16:43:00 -0400

    Questão 11 concluida

---
+++ Questão-11.js
@@ -0,0 +1,7 @@
+/* 
+Questão 11: Qual é a diferença entre let, const e var em JavaScript?
+*/
+
+let nome; // Funciona em Escopo de bloco = conteudo especifico dentro de um bloco (bloco = {}
+var idade; // Funciona em Escopo global = consegue ser visto em todo o programa
+const PI = 3.14; // Funciona como um Escopo de bloco qunado dentro de um bloco, e de maneira global quando for fora de um bloco, sendo um valor imutavel
