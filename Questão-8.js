bac453a433cf7f2084451bd1cfaa1b43458daf97
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-18 16:41:30 -0400

    5, 6, 8, atualizadas

--- Questão-8.js
+++ Questão-8.js
@@ -2,4 +2,9 @@
 Questão 8: Como você pode acessar o último elemento de um array em JavaScript?
 */
 
-let arrayExemplo = [1, 2, 3, 4, 5]; // "arrayExemplo" é um array por armazena uma coleção de elementos, sendo geralmente do mesmo tipo
+let arrayExemplo = ["Maça", "laranja"]; // "arrayExemplo" é um array por armazena uma coleção de elementos, sendo geralmente do mesmo tipo
+
+        let arrayCont       = arrayExemplo.length - 1;
+/* valor do ultimo elemento = soma dos elementos  - 1 | Por causa que o array conta seus elemento de 0 para frente, e a length e de 1 para frente */
+
+console.log(arrayExemplo[arrayCont])
