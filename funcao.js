a6c369de9883a597180f8cac65f56e3546bb7cbb
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-18 16:39:25 -0400

    Função adicionada

---
+++ funcao.js
@@ -0,0 +1,38 @@
+function saudacao(nome) { /*nome = Parametro*/
+
+    return "ola, " + nome + "!";
+
+}
+
+console.log(saudacao("Nikael"));
+
+
+    //função: pode ou não ter parametro mas sempre retorna
+    
+    /*
+    //procedimentos: pode ou não ter parametro mas não pode retorna
+    //ex:
+    function mensagem(){
+        
+        console.log("Esta é uma mensagem")
+    }*/
+
+    /*
+    //array:
+    let frutas = ["Maça","Banana","Laranja"] //frutas ira me dar todas as frutas
+    //indices:
+    console.log(frutas[1]) //tem saida em Banana por comessar contando de 0
+    */
+    
+    /*
+    //objeto
+    //ex:
+    Objeto: let pessoa = {
+       nome:"João",
+       idade: 30,
+       profissao:"Engenheiro"
+    
+    console.log(pessoa.nome);
+    console.log(pessoa.idade);
+    console.log(pessoa.profissao);
+    */
