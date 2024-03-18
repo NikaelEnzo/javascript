aa86fbe042498f302798337a20b346299f720bf3
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-18 16:42:40 -0400

    Questão 10 concluida

---
+++ Questão-10.js
@@ -0,0 +1,17 @@
+/* 
+Questão 10: Como você pode remover o último elemento de um array em 
+JavaScript?
+*/
+
+let frutas = ["Maça","Banana","Laranja", "Kiwi"] //frutas ira me dar todas as frutas
+
+frutas.pop(); // pop apaga o ultimo
+console.log(frutas);
+
+/* tambem funciona
+
+let arrayCont = frutas.length - 1;
+
+delete frutas[arrayCont]
+
+console.log(frutas) */
