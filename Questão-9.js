55bb494b304220161f00da84bbe3cb7f4e57f865
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-18 16:42:19 -0400

    Questão 9 concluida

---
+++ Questão-9.js
@@ -0,0 +1,10 @@
+/*
+Questão 9: Como você pode adicionar um novo elemento ao final de um array 
+em JavaScript?
+*/
+
+    let frutas = ["Maça","Banana","Laranja"] //frutas ira me dar todas as frutas
+
+    frutas.push("Kiwi")
+
+    console.log(frutas[3]) //tem saida em Banana por comessar contando de 0
