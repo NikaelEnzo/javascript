775e117230d0f9d0c9efb8cd26818f821c8c6046
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 16:56:52 -0400

    Questão 17 concluida

---
+++ Questão-17.js
@@ -0,0 +1,10 @@
+/* 
+Questão 17: Como você pode adicionar uma propriedade a um objeto em 
+JavaScript? 
+*/
+
+var album = { //objeto e feito de uma propriedade e um valor
+    title: "Metallica (Black Album)",
+//propriedade        valor
+    released: 1991,
+}
