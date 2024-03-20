96a9ae24a82cc5c2d9b7038889433b5776009692
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 16:56:26 -0400

    Questão 15 concluida

---
+++ Questão-15.js
@@ -0,0 +1,10 @@
+/* 
+Questão 15: Como você pode obter o comprimento de uma string em 
+JavaScript?
+*/
+
+let texto = "O como o roa tirou 0 em geografia?"
+
+let cont = texto.length
+
+console.log(cont)
