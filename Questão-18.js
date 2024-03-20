91148a0b2ce83e85cc9e0e9ccc6a534c6408efb2
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 16:57:05 -0400

    Questão 18 concluida

---
+++ Questão-18.js
@@ -0,0 +1,12 @@
+/* 
+Questão 18: Como você pode acessar o valor de uma propriedade de um 
+objeto em JavaScript? 
+*/
+
+var album = { //objeto e feito de uma propriedade e um valor
+    title: "Metallica (Black Album)",
+//propriedade        valor
+    released: 1991,
+    }
+
+console.log(album.title); // acessar valor de title
