ac9369ee06a92c4276c2f3e80ee62ef2d7ae37d8
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 16:58:15 -0400

    Questão 23 concluida

---
+++ Questão-23.js
@@ -0,0 +1,10 @@
+/* 
+Questão 23: Como você pode criar um loop while em JavaScript? 
+*/
+
+var contador = 1;
+
+while (contador <= 5) {
+    console.log("Contador: " + contador);
+    contador++;
+}
