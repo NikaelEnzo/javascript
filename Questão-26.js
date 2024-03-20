e5b85ef04a0149824d2c0a2f74ede43e828d4969
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 17:28:40 -0400

    Questão 26 concluida

---
+++ Questão-26.js
@@ -0,0 +1,8 @@
+/* 
+Questão 26: Como você pode usar o operador ternário em JavaScript? 
+*/
+
+var notaDoAluno = 8;
+var avaliacao = notaDoAluno > 6 ? "aprovado" : "reprovado";
+                            //  ? verifica se 6 é < a notaDoAluno, se sim "aprovado" se não "reprovado"
+console.log(avaliacao) // output: "aprovado"
