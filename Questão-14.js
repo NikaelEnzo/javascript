bc72b07cc32532487a73548c751d2e075bbd64de
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 16:56:12 -0400

    Questão 14 concluida

---
+++ Questão-14.js
@@ -0,0 +1,11 @@
+/* 
+Questão 14: Como você pode converter uma string em minúsculas em 
+JavaScript?
+*/
+
+let texto = "OlA BATaTa"
+
+let minusculo = texto.toLowerCase()
+
+console.log(texto)
+console.log(minusculo)
