3d5d2eaae0d9c7b38a5edb36d4cf80db88d0ca6e
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 17:28:23 -0400

    Questão 22 atualizada

--- Questão-22.js
+++ Questão-22.js
@@ -4,8 +4,6 @@ Questão 22: Como você pode verificar se uma variável é nula em JavaScript?
 
 let variavel = null;
 
-console.log(variavel?.propriedade) // verifica se a propriedade “variavel” existe no objeto, Caso o objeto seja nulo, o valor 'undefined' será retornado
-
 if (variavel === null){
     console.log("true")
 }
