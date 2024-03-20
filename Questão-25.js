66e1ab11ea32648c90d300cef9e1972704e89415
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-20 17:02:09 -0400

    Questão 25 concluida

---
+++ Questão-25.js
@@ -0,0 +1,9 @@
+/* 
+Questão 25: Como você pode declarar uma função anônima em JavaScript?
+*/
+
+let termo = function() { // função sen ser declarada é uma função anonima
+    console.log("função anonima");
+}
+
+termo();
