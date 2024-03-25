801511e51e77821f394201132f232c195ae44296
NikaelEnzo <enzo.marthins@gmail.com>
2024-03-25 16:52:08 -0400

    Questão 29 concluida

---
+++ Questão-29.js
@@ -0,0 +1,10 @@
+/* 
+Questão 29: Como você pode converter um número em uma string em 
+JavaScript?
+*/
+
+let numero = 789
+let string789 = String(numero)
+
+console.log(string789);
+console.log(typeof string789);
