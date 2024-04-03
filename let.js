37fa1839c607af0fa7a0de9bde547b9067f9f002
NikaelEnzo <enzo.marthins@gmail.com>
2024-04-03 13:48:17 -0400

    Let atualizado

--- let.js
+++ let.js
@@ -1,7 +1,7 @@
 // Declaração de uma variável
 let nome; // Escopo de bloco = conteudo dentro de {}
 var idade; // Escopo global = ver em todo o programa sendo o valor imutavel
-const PI = 3.14; // Escopo de bloco dentro de um bloco e global fora de um bloco 
+const PI = 3.14; // Funciona como um escopo de bloco dentro de um bloco e como escopo global fora de um bloco 
 
 
 function soma() {
